350a073919b3b57e200f040ebee73a4384ccf133
alexsommers7 <asommers22@hotmail.com>
2021-07-10 18:02:54 -0700

    hero image shadow fix

--- src/components/home/children/Hero.vue
+++ src/components/home/children/Hero.vue
@@ -110,14 +110,14 @@ section.hero {
     &::after {
       content: "";
       display: block;
-      width: 100%;
-      height: 15%;
+      width: 86%;
+      height: 30%;
       position: absolute;
       bottom: 0;
       left: 50%;
       z-index: 20;
-      transform: translateX(-50%);
-      background-image: linear-gradient(180deg, rgba(18, 18, 18, 0) 1%, $color-background 50%);
+      transform: translate(-50%, 30%);
+      background-image: linear-gradient(180deg, rgba(18, 18, 18, 0) 1%, $color-background 37%);
     }
 
     .photo {
