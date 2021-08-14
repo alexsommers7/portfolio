fc0fd26c625dc0ff65539d56a7153b907fcd71d5
alexsommers7 <asommers22@hotmail.com>
2021-08-13 17:29:28 -0700

    copy update for about section

--- src/components/home/children/About.vue
+++ src/components/home/children/About.vue
@@ -10,8 +10,8 @@
       </p>
       <p>I'm a <span class="color-primary">Front-End Web Developer</span> based out of Tempe, Arizona.</p>
       <p>
-        In both freelance and professional settings, I harness the power of modern web technologies to craft memorable,
-        accessible user experiences.
+        In both freelance and professional settings, I harness the power of modern web technologies to create
+        interactive, accessible user experiences.
       </p>
     </div>
     <div class="about__lately">
