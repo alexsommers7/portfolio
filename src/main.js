fc5fe67e6480da88bf745ee6a2a85e7adb92642a
alexsommers7 <asommers22@hotmail.com>
2021-09-04 11:56:33 -0700

    safari scroll bug test yet again

--- src/main.js
+++ src/main.js
@@ -9,51 +9,22 @@ import formFailure from "./views/FormFailure";
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
-  mode: "history",
   routes: [
-    {
-      path: "/",
-      name: "Home",
-      component: Homepage,
-      meta: {
-        NoLoadIn: false,
-        noScrollArrow: false,
-        scrollPos: {
-          top: 0,
-          left: 0,
-        },
-      },
-    },
+    { path: "/", name: "Home", component: Homepage, meta: { NoLoadIn: false, noScrollArrow: false } },
     {
       path: "/project-showcase",
       name: "Projects",
       component: Projects,
-      meta: {
-        NoLoadIn: true,
-        noScrollArrow: true,
-        scrollPos: {
-          top: 0,
-          left: 0,
-        },
-      },
+      meta: { NoLoadIn: true, noScrollArrow: true },
     },
     { path: "/thanks", name: "Thanks!", component: formSuccess, meta: { NoLoadIn: true, noScrollArrow: true } },
     { path: "/error", name: "", component: formFailure, meta: { NoLoadIn: true, noScrollArrow: true } },
     { path: "*", name: "", component: Homepage, meta: { NoLoadIn: true, noScrollArrow: false } },
   ],
-  scrollBehavior(to, from, savedPosition) {
-    // scroll to top if on same route
-    if (to.name === from.name) {
-      to.meta?.scrollPos && (to.meta.scrollPos.top = 0);
-      return { left: 0, top: 0 };
-    }
-    const scrollpos = savedPosition || to.meta?.scrollPos || { left: 0, top: 0 };
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(scrollpos);
-      }, 600); // transition just before 600ms
-    });
+  scrollBehavior() {
+    return { x: 0, y: 0 };
   },
+  mode: "history",
 });
 
 new Vue({
