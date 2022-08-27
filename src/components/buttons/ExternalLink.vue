83eb62ba9966a39e34313c9fbd459bbf46b75643
alexsommers7 <asommers22@hotmail.com>
2022-08-27 10:00:14 -0700

    add external link component back to resolve netlify build error due to case sensitivity

---
+++ src/components/buttons/ExternalLink.vue
@@ -0,0 +1,33 @@
+<template>
+  <a :href="targetLink" class="btn" :class="classes" target="_blank" rel="noopener" :aria-label="ariaLabel">
+    {{ content }}
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'ExternalLink',
+  props: {
+    classes: {
+      type: String,
+      default: '',
+    },
+    content: {
+      type: String,
+      default: '',
+    },
+    type: {
+      type: String,
+      default: 'button',
+    },
+    targetLink: {
+      type: String,
+      default: '#',
+    },
+    ariaLabel: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
