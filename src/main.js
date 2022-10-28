07f7db8e9d88c5c0c0d832b7d3a48cf2ae04c9d8
alexsommers7 <asommers22@hotmail.com>
2022-10-27 17:40:08 -0700

    resovle issue with anchors on page load not behaving as expected

--- src/main.js
+++ src/main.js
@@ -21,7 +21,13 @@ export const router = new VueRouter({
     { path: '/error', name: '', component: formFailure, meta: { noScrollArrow: true } },
     { path: '*', name: '', component: Homepage, meta: { noScrollArrow: false } },
   ],
-  scrollBehavior() {
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    if (to.hash) {
+      return { selector: to.hash };
+    }
     return { x: 0, y: 0 };
   },
   mode: 'history',
