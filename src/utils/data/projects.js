758119e229317c8fbffaaec5eadf95a629faee02
alexsommers7 <asommers22@hotmail.com>
2022-10-27 17:28:52 -0700

    content updates

--- src/utils/data/projects.js
+++ src/utils/data/projects.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: 'StorePI',
     description:
-      'An open-source REST API that was created for use in e-commerce prototyping and Front-End Developer talent evaluations. Features product, user, cart, and authentication resources plus more.',
+      'An open-source API that was created for use in e-commerce prototyping and Front-End Developer talent evaluations. Features product, user, cart, and authentication resources plus more.',
     tools: ['MongoDB', 'Express', 'React', 'Node', 'Mongoose', 'Chakra UI'],
     primaryCTA: 'View Docs',
     primaryLink: 'https://storepi-docs.netlify.app/docs',
