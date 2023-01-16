b05b5bc4d32d5a2741236f1ad360db25cd6e451e
alexsommers7 <asommers22@hotmail.com>
2023-01-16 16:17:00 -0700

    Reorder StorePI tools

--- src/utils/data/projects.js
+++ src/utils/data/projects.js
@@ -3,7 +3,7 @@ export const projects = [
     title: 'StorePI',
     description:
       'An open-source API that was created for use in e-commerce prototyping and Front-End Developer talent evaluations. Features product, user, cart, and authentication resources plus more.',
-    tools: ['React', 'Typescript', 'Node', 'MongoDB', 'Mongoose', 'Express', 'Chakra UI', 'Heroku'],
+    tools: ['React', 'Typescript', 'Chakra UI', 'Node', 'MongoDB', 'Mongoose', 'Express', 'Heroku'],
     primaryCTA: 'View Docs',
     primaryLink: 'https://storepi.herokuapp.com/docs',
     secondaryCTA: 'View Code',
