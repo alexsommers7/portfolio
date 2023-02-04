3c2216ba90816773d58eeb1de86824c82fa6b7c6
alexsommers7 <asommers22@hotmail.com>
2023-02-04 16:07:10 -0700

    Update StorePI project for GraphQL release

--- src/utils/data/projects.js
+++ src/utils/data/projects.js
@@ -3,7 +3,7 @@ export const projects = [
     title: 'StorePI',
     description:
       'An open-source API that was created for use in e-commerce prototyping and Front-End Developer talent evaluations. Features product, user, cart, and authentication resources plus more.',
-    tools: ['React', 'Typescript', 'Chakra UI', 'Node', 'MongoDB', 'Mongoose', 'Express', 'Heroku'],
+    tools: ['React', 'Typescript', 'Chakra UI', 'Node', 'MongoDB', 'Mongoose', 'Express', 'GraphQL'],
     primaryCTA: 'View Docs',
     primaryLink: 'https://storepi.herokuapp.com/docs',
     secondaryCTA: 'View Code',
