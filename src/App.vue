b113720704827b1d2e5b3c83eb36ae15dfdc09d2
alexsommers7 <asommers22@hotmail.com>
2023-12-10 16:13:14 -0700

    Refactor App component to composition api

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,66 @@
+<script setup>
+  import { ref, provide, onMounted, onUnmounted, watch, nextTick } from 'vue';
+  import { useRoute, useRouter } from 'vue-router';
+  import Nav from '@/components/layout/Nav.vue';
+  import Sidebar from '@/components/layout/Sidebar.vue';
+  import Footer from '@/components/layout/Footer.vue';
+
+  const route = useRoute();
+  const router = useRouter();
+
+  const mainEl = ref(null);
+  const onMobile = ref(false);
+
+  provide('onMobile', onMobile);
+
+  const observerTargets = ref([]);
+  const observeSections = ref(true);
+  const noScrollArrow = router.resolve(route).meta.noScrollArrow || false;
+
+  const checkScreenSize = () => {
+    onMobile.value = Math.max(document.documentElement.clientWidth, window.innerWidth || 0) < 1200;
+  };
+
+  const updateTargets = () => {
+    observeSections.value = true;
+    observerTargets.value = [...mainEl.value.querySelectorAll('section.track')];
+  };
+
+  const fixCrappyScrollBehavior = () => {
+    // safari scroll jump bug
+    mainEl.value.scrollIntoView();
+  };
+
+  onMounted(() => {
+    checkScreenSize();
+    updateTargets();
+    window.addEventListener('resize', checkScreenSize);
+  });
+
+  onUnmounted(() => {
+    window.removeEventListener('resize', checkScreenSize);
+  });
+
+  watch(
+    () => route.name,
+    (to, from) => {
+      if (to !== from && !route.hash) {
+        fixCrappyScrollBehavior();
+      }
+
+      nextTick(() => {
+        route.name === 'Home' ? updateTargets() : (observeSections.value = false);
+      });
+    }
+  );
+</script>
+
 <template>
   <div>
-    <Nav :onMobile="onMobile"></Nav>
+    <Nav />
 
     <transition appear appear-to-class="opacity-1" appear-active-class="opacity-0">
-      <main id="main" ref="main">
+      <main id="main" ref="mainEl">
         <router-view></router-view>
       </main>
     </transition>
@@ -11,63 +68,11 @@
     <Sidebar
       :observerTargets="observerTargets"
       :observeSections="observeSections"
-      :onMobile="onMobile"
       :noScrollArrow="noScrollArrow"
     ></Sidebar>
 
     <transition appear appear-to-class="opacity-1" appear-active-class="opacity-0">
-      <Footer></Footer>
+      <Footer />
     </transition>
   </div>
 </template>
-
-<script>
-  import Nav from '@/components/layout/Nav.vue';
-  import Sidebar from '@/components/layout/Sidebar.vue';
-  import Footer from '@/components/layout/Footer.vue';
-
-  export default {
-    name: 'App',
-    data() {
-      return {
-        observerTargets: [],
-        observeSections: true,
-        onMobile: false,
-        noScrollArrow: this.$router.resolve(this.$route).meta.noScrollArrow || false,
-      };
-    },
-    methods: {
-      checkScreenSize() {
-        this.onMobile =
-          Math.max(document.documentElement.clientWidth, window.innerWidth || 0) < 1200;
-      },
-      updateTargets() {
-        this.observeSections = true;
-        this.observerTargets = [...this.$refs.main.querySelectorAll('section.track')];
-      },
-      fixCrappyScrollBehavior() {
-        // fixing safari scroll jump bug
-        this.$refs.main.scrollIntoView();
-      },
-    },
-    mounted() {
-      this.checkScreenSize();
-      window.addEventListener('resize', this.checkScreenSize);
-      this.updateTargets();
-    },
-    watch: {
-      $route(to, from) {
-        if (to.name !== from.name && !to.hash) this.fixCrappyScrollBehavior();
-
-        this.$nextTick(() => {
-          this.$route.name === 'Home' ? this.updateTargets() : (this.observeSections = false);
-        });
-      },
-    },
-    components: {
-      Nav,
-      Sidebar,
-      Footer,
-    },
-  };
-</script>
