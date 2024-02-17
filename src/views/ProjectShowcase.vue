00b79c0a8b08e404bc7d0b8c1237b231cb19de8d
alexsommers7 <asommers22@hotmail.com>
2024-02-17 15:53:03 -0700

    chore: clean up showcase entry animation

--- src/views/ProjectShowcase.vue
+++ src/views/ProjectShowcase.vue
@@ -1,47 +1,11 @@
 <script setup>
-  import { ref, onMounted } from 'vue';
   import { projects } from '@/utils/data/projects';
   import ProjectCard from '@/components/cards/ProjectCard.vue';
-  import { gsap } from 'gsap';
-  import { ScrollTrigger } from 'gsap/ScrollTrigger';
-
-  gsap.registerPlugin(ScrollTrigger);
-
-  const sectionEl = ref(null);
-
-  const configureScrollTrigger = () => {
-    gsap.set('.project-showcase article.project', { y: 100, opacity: 0 });
-
-    ScrollTrigger.batch('.project-showcase article.project', {
-      start: 'top 85%',
-      onEnter: batch =>
-        gsap.to(batch, {
-          opacity: 1,
-          duration: 1.1,
-          y: 0,
-          stagger: { each: 0.25 },
-          overwrite: true,
-          ease: 'expo.out',
-        }),
-    });
-
-    ScrollTrigger.addEventListener('refreshInit', () => {
-      if (!sectionEl.value.querySelector('.project-showcase article.project')) {
-        return;
-      }
-
-      gsap.set('.project-showcase article.project', { y: 0, opacity: 1 });
-    });
-  };
-
-  onMounted(() => {
-    configureScrollTrigger();
-  });
 </script>
 
 <template>
   <transition appear appear-to-class="opacity-1" appear-active-class="opacity-0">
-    <section class="projects project-showcase full-width" ref="sectionEl">
+    <section class="projects project-showcase full-width">
       <div class="headings">
         <h1 class="heading heading--3">PROJECTS</h1>
         <h2 class="heading heading--4 heading--section">Interfaces for the Modern World</h2>
