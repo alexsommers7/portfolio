4f4d772d2822215706841cd6f7858e207f38987f
alexsommers7 <asommers22@hotmail.com>
2024-04-28 20:51:43 -0700

    refactor: Remove self link title

--- src/components/cards/ProjectCard.vue
+++ src/components/cards/ProjectCard.vue
@@ -36,16 +36,7 @@
 
     <div class="btns">
       <ExternalLink
-        v-if="project.isSelf"
-        class="btn--primary"
-        title="Big fan of recursion eh?"
-        @click.prevent
-      >
-        {{ project.primaryCTA }}
-      </ExternalLink>
-
-      <ExternalLink
-        v-else-if="project.primaryCTA"
+        v-if="!project.isSelf"
         class="btn--primary"
         :targetLink="project.primaryLink"
         :aria-label="getAriaLabel(project, project.primaryCTA)"
