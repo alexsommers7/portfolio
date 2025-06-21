28930d8c792fbdb62836929189e7c7d166fe1b19
alexsommers7 <asommers22@hotmail.com>
2025-06-20 23:23:24 -0400

    refactor: Add minor to degree listing

--- utils/data/education.js
+++ utils/data/education.js
@@ -1,7 +1,7 @@
 export const education = [
   {
     id: 1,
-    title: 'B.A. - Marketing',
+    title: 'B.A in Sport Management, Minor in Marketing',
     company: 'Baldwin Wallace University',
     location: 'Berea, OH',
     timeframe: 'August 2013 - May 2017',
